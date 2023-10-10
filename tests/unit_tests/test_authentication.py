71e114fb21e4da10bbabf0a5360b1e642a89b75f
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2023-10-10 15:21:09 +0200

    Clean code to stick to PEP8

--- tests/unit_tests/test_authentication.py
+++ tests/unit_tests/test_authentication.py
@@ -6,15 +6,15 @@ from server import app, clubs
 class TestAuthentication:
     def setup_method(self):
         self.client = app.test_client()
-    
+
     def test_registration_with_empty_email(self):
         result = self.client.post("/showSummary", data={"email": ""})
         assert result.status_code == 400
-    
+
     def test_registration_with_unknown_email(self):
         result = self.client.post("/showSummary", data={"email": "jane@doe.cc"})
         assert result.status_code == 401
-    
+
     def test_registration_with_known_email(self):
         result = self.client.post("/showSummary", data={"email": clubs[0]["email"]})
         assert result.status_code == 200
