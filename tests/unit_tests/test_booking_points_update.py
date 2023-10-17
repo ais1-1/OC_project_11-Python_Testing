ba7e3d6c2af50ce711aceffc4296dbe4df81f001
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2023-10-17 10:49:19 +0200

    Add test for ending with negative points and rename an old test to clarify

--- tests/unit_tests/test_booking_points_update.py
+++ tests/unit_tests/test_booking_points_update.py
@@ -6,7 +6,7 @@ import server
 class TestBookingPointsUpdate:
     def setup_method(self):
         self.client = server.app.test_client()
-        self.club = [{"name": "Test club", "email": "test@example.com", "points": "13"}]
+        self.club = [{"name": "Test club", "email": "test@example.com", "points": "8"}]
         self.competition = [
             {
                 "name": "Test Festival",
@@ -14,13 +14,13 @@ class TestBookingPointsUpdate:
                 "numberOfPlaces": "25",
             }
         ]
-        self.booking_details = {"Test Festival": {"Test club": 7}}
+        self.booking_details = {"Test Festival": {"Test club": 0}}
 
         server.clubs = self.club
         server.competitions = self.competition
         server.booking_details = self.booking_details
 
-    def test_should_subtract_points_on_purchasing_places(self):
+    def test_subtract_points_on_purchasing_places(self):
         places_required = 4
         club_name = self.club[0]["name"]
         club_points = int(self.club[0]["points"])
@@ -34,3 +34,20 @@ class TestBookingPointsUpdate:
             },
         )
         assert int(self.club[0]["points"]) == club_points - places_required
+
+    def test_deduct_more_points_than_available(self):
+        places_required = 9
+        club_name = self.club[0]["name"]
+        competition_name = self.competition[0]["name"]
+        response = self.client.post(
+            "/purchasePlaces",
+            data={
+                "competition": competition_name,
+                "club": club_name,
+                "places": places_required,
+            },
+        )
+        error_message = "have enough points to do this"
+        assert response.status_code == 400
+        assert int(self.club[0]["points"]) >= 0
+        assert error_message in response.data.decode()
