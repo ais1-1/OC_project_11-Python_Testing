e15f24b8ff84dba6d5fcd8aee056af47314a47e8
Aiswarya Kaitheri Kandoth <aiswaryakk29@gmail.com>
2023-10-17 12:05:44 +0200

    Add test for points display route

---
+++ tests/unit_tests/test_routes.py
@@ -0,0 +1,27 @@
+""" Unit tests for app routes """
+
+import server
+
+
+class TestRoutes:
+    def setup_method(self):
+        self.client = server.app.test_client()
+        self.club = [{"name": "Test club", "email": "test@example.com", "points": "13"}]
+        self.competition = [
+            {
+                "name": "Test Festival",
+                "date": "2020-03-27 10:00:00",
+                "numberOfPlaces": "25",
+            }
+        ]
+        self.booking_details = {"Test Festival": {"Test club": 7}}
+
+        server.clubs = self.club
+        server.competitions = self.competition
+        server.booking_details = self.booking_details
+
+    def test_view_club_points_route(self):
+        url = "/viewClubPoints"
+        response = self.client.get(url)
+        assert "Points Display Board" in response.data.decode()
+        assert response.status_code == 200
